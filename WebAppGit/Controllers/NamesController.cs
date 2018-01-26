5ff0a5959e4a127fc02365d41a9ec5e9dbde1e5c
Randy Whitaker <whitaker.randy@gmail.com>
2018-01-25 20:53:25 -0600

    Added Name Controller

---
+++ WebAppGit/Controllers/NamesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAppGit.Controllers
+{
+  public class NamesController : ApiController
+  {
+    public string[] get()
+    {
+      return new string[] { "Tim", "Bob", "Sam", "All", "Dan", "Car", "Cat" };
+    }
+  }
+}
